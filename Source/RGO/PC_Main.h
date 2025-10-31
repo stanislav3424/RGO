3fc4437e76cdf685946756345a4a1749992c6fcf
Stanislav <doudoladov_stanislav@mail.ru>
2025-10-31 12:45:33 +0300

    .05

---
+++ Source/RGO/PC_Main.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/PlayerController.h"
+#include "PC_Main.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class RGO_API APC_Main : public APlayerController
+{
+	GENERATED_BODY()
+	
+};
