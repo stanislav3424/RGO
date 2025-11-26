ca92d987b9236ad0958429d961f9291166a20456
Stanislav <doudoladov_stanislav@mail.ru>
2025-11-26 14:52:46 +0300

    10.1

---
+++ Source/RGO/AIC_Base.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "DetourCrowdAIController.h"
+#include "AIC_Base.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class RGO_API AAIC_Base : public ADetourCrowdAIController
+{
+	GENERATED_BODY()
+	
+};
