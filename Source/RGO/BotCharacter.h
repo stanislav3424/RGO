25941022edb919a13cf0f6232d27ccbfbb90788f
Stanislav <dudoladov_stanislav@mail.ru>
2025-12-06 10:58:03 +0300

    22

---
+++ Source/RGO/BotCharacter.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "BaseCharacter.h"
+#include "BotCharacter.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class RGO_API ABotCharacter : public ABaseCharacter
+{
+	GENERATED_BODY()
+	
+};
