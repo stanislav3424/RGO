e539aa7dc0a96d0318494d34cdcceaf11a0fc019
Stanislav <dudoladov_stanislav@mail.ru>
2025-12-08 17:18:59 +0300

    66

---
+++ Source/RGO/UW_BarBase.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "UW_BarBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class RGO_API UUW_BarBase : public UUserWidget
+{
+	GENERATED_BODY()
+	
+};
